57f77da8bc3e0ee139d9b57d04ebc800f379c741
shobhan126 <shobhan126@gmail.com>
2023-04-16 21:20:49 -0700

    wip faster multiplication w/ inbounds

--- src/paulis.jl
+++ src/paulis.jl
@@ -3,215 +3,259 @@ Module defining Pauli Operators
     $(EXPORTS)
 """
 module Paulis
-    # Pauli Primitive Enum
-    import Base: show, *, -, ==, abs, vec
-    import Base.Iterators.product
-    using SparseArrays: SparseMatrixCSC
-    using DocStringExtensions
+# Pauli Primitive Enum
+import Base: show, *, -, ==, abs, vec
+import Base.Iterators.product
 
-    include("paulitable.jl")
+using DocStringExtensions
+import LinearAlgebra: mul!, lmul!, rmul!, kron
+using LoopVectorization
+include("paulitable.jl")
 
+#### Common Interface for different Representations #####
+abstract type AbstractPauli{N} end
+
+### Accessors 
+function xbits(p::AbstractPauli) end
+function zbits(p::AbstractPauli) end
+function bits(p::AbstractPauli) end
+function signbit(p::AbstractPauli) end
+function imagbit(p::AbstractPauli) end
+
+### Math Operations ####
+function Base.adjoint(p::AbstractPauli) end 
+function Base.:-(x::AbstractPauli) end
+function Base.:*(p::AbstractPauli, q::AbstractPauli) end 
+function Base.:*(y::AbstractPauli, x::Number) end
+function Base.:*(x::Number, y::AbstractPauli) end
+function abs(p::AbstractPauli) end
+function commuting(x::AbstractPauli, y::AbstractPauli) end
+function anticommuting(x::AbstractPauli, y::AbstractPauli) end
+function ==(x::AbstractPauli, y::AbstractPauli) end
+
+#### Inplace Operations
+function lmul!(a::Number, B::AbstractPauli) end
+function rmul!(A::AbstractPauli, b::Number) end
+function lmul!(A::AbstractPauli, B::AbstractPauli) end
+function rmul!(A::AbstractPauli, B::AbstractPauli) end
+function mul!(C::AbstractPauli, A::AbstractPauli, B::AbstractPauli, α::Number, β::Number) end
+function mul!(Y::AbstractPauli, A::AbstractPauli, B::AbstractPauli) end
+
+
+# Common Operations
+
+# REPL representation for the Pauli Operator
+function Base.show(io::IO, p::AbstractPauli)
+    prefix = signbit(p) ? "-" : ""
+    prefix *= imagbit(p) ? "im" : ""
+    print(io, prefix)
+    s = ["I", "X", "Z", "Y"]
+    foreach(x -> print(io, s[x+1]), xbits(p) + 2 * zbits(p))
+end
+
+isneg(p::AbstractPauli) = signbit(p)
+
+
+# """
+# Create an N-Qubit Pauli Operator
+#     $(TYPEDEF)
+# with fields:
+#     $(FIELDS)
+
+# # Examples
+# ```julia-repl
+# julia> Pauli(:XIZI)
+# XIZI
+# ```
+# """
+mutable struct Pauli{N} <: AbstractPauli{N}
     """
-    Create an N-Qubit Pauli Operator
-        $(TYPEDEF)
-    with fields:
-        $(FIELDS)
-
-    # Examples
-    ```julia-repl
-    julia> Pauli(:XIZI)
-    XIZI
-    ```
+        true if coefficient has `-` sign
     """
-    struct Pauli
-        """
-            true if coefficient has `-` sign
-        """
-        signbit::Bool
-        """
-            true if coefficient is imaginary
-        """
-        imagbit::Bool
-        """
-            Sparse Bit Representation of the Pauli Operator.
-        """
-        bits::SparseMatrixCSC
-    end
-
-
-    isneg(p::Pauli) = p.signbit
-
-
+    signbit::Bool
     """
-    Create a Pauli Operator from its symplectic representation [xbits; zbits]
+        true if coefficient is imaginary
+    """
+    imagbit::Bool
+    """
+        Sparse X-Bit Representation of the Pauli Operator.
+    """
+    xbits::BitArray{1}
+    zbits::BitArray{1}
+end
+
+
+"""
+Create a Pauli Operator from its symplectic representation [xbits; zbits]
+
+    $(FUNCTIONNAME)(xibts, zbits, neg=false, imag=false)
+
+"""
+Pauli(xbits::AbstractVector, zbits::AbstractVector, signbit=false, imagbit=false) = Pauli{length(xbits)}(signbit, imagbit, BitArray(xbits), BitArray(zbits))
+
+
+"""
+Create a Pauli Operator from its symplectic representation [xbits; zbits]
     
-        $(FUNCTIONNAME)(xibts, zbits, neg=false, imag=false)
+    $(FUNCTIONNAME)(xz, neg=false, imag=false)
+"""
+Pauli(xz::AbstractVector, neg=false, imag=false) = Pauli(xz[1:Int(length(x) / 2)], xz[Int(length(x) / 2)+1:end], neg, imag)
+
+
+"""
+Macro to instantiate a Pauli{N} type from a string
+    @p_str("IXYZ")
+    p"IXYZ..."
+"""
+macro p_str(p)
+    conv = Dict(
+        'x' => [true, false],
+        'y' => [true, true],
+        'z' => [false, true],
+        'i' => [false, false]
+    )
+    m = BitArray(hcat([conv[lowercase(v)] for v in p]...)')
+    return Pauli(m[:,1], m[:,2])
+end
+
+"""
+@pauli XYZXII...
+
+Convenience macro for constructing a Pauli{N} type.
+"""
+macro pauli(ex)
+    s = string(ex)
+    :(@p_str $s)
+end
+
+"""
+Construct a Puali type by passing a symbol.
     
-    """
-    function Pauli(x::AbstractVector, z::AbstractVector, neg=false, imag=false)
-        Pauli(neg, imag, SparseMatrixCSC(BitMatrix(hcat(x,z))))
-    end
+    $(FUNCTIONNAME)(sym)
 
+"""
+function Pauli(sym::Symbol)
+    eval(:(@pauli $sym))
+end
 
-    """
-    Create a Pauli Operator from its symplectic representation [xbits; zbits]
-        
-        $(FUNCTIONNAME)(xz, neg=false, imag=false)
-    """
-    Pauli(xz::AbstractVector, neg=false, imag=false) = Pauli(xz[1:Int(length(x)/2)], xz[Int(length(x)/2)+1:end], neg, imag)
-
-
-
-    """
-    Construct a Puali type by passing a symbol.
-        
-        $(FUNCTIONNAME)(sym)
-
-    """
-    function Pauli(sym::Symbol)
-        eval(:(@pauli $sym))
-    end
-
-    """
-    Construct a Pauli Operator type by passing a string representation
-        
-        $(FUNCTIONNAME)(s)
-        
-    """
-    function Pauli(s::AbstractString)
-        eval(:(@p_str $s))
-    end
-
-
-    """
-    Bit Matrix Representation of Pauli Operator.
-
-        $(FUNCTIONNAME)(p)
-    """
-    bits(p::Pauli) = p.bits
-    xbits(p::Pauli) = bits(p)[:,1]
-    zbits(p::Pauli) = bits(p)[:,2]
-
-    """
-    Take a adjoint of the pauli operator `iXYZ...`  => `-iXYZ...`.
-
-        $(FUNCTIONNAME)(p::Pauli)
-    """
-    Base.adjoint(p::Pauli) = p.imagbit ? Pauli(p.signbit ⊻ true, p.imagbit, bits(p)) : p
-
-
-    # REPL representation for the Pauli Operator
-    function Base.show(io::IO, p::Pauli)
-        prefix = p.signbit ? "-" : ""
-        prefix *= p.imagbit ? "im" : ""
-        print(io, prefix)
-        s = ["I", "X", "Z",  "Y"]
-        foreach(x-> print(io, s[x+1]), xbits(p) + 2*zbits(p))    
-    end
-
-
-    function Base.:*(p::Pauli, q::Pauli)
-        newbits = bits(p) .⊻ bits(q)
-        coeff = (im)^(p.imagbit+q.imagbit) * prod([levicivita(i,j) for (i, j) in zip(eachrow(p.bits), eachrow(q.bits))])
-        Pauli((coeff.re + coeff.im < 0) ⊻ p.signbit ⊻ q.signbit, ~isreal(coeff), newbits)
-    end
-
-    # Scalar Multiplication
-    *(y::Pauli, x::Number) = Pauli((x < 0) ⊻ y.signbit , y.imagbit, y.bits)
-    *(y::Pauli, x::Complex) = isequal(x,im) ? 
-        Pauli(y.signbit ⊻ y.imagbit, ~y.imagbit, y.bits) : isequal(x,-im) ? 
-        Pauli(~y.signbit ⊻ y.imagbit, ~y.imagbit, y.bits) : throw(MethodError)
-    *(x::Number, y::Pauli) = *(y, x)
+"""
+Construct a Pauli Operator type by passing a string representation
     
+    $(FUNCTIONNAME)(s)
     
-    #### Negative sign -Pauli(1, [...]) = Pauli(-1, [...])
-    Base.:-(x::Pauli) = Pauli(x.signbit ⊻ true, x.imagbit, x.bits)
-
-    """
-    Return a new Pauli with the coefficient of `p` set to 1
-        abs(p::Pauli)
-    """
-    abs(p::Pauli) = Pauli(false, false, p.bits)
-
-    ==(x::Pauli, y::Pauli) = (x.signbit == y.signbit) & isequal(bits(x), bits(y)) & (x.imagbit == y.imagbit)
+"""
+Pauli(s::AbstractString) = eval(:(@p_str $s))
 
 
-    """
-    Macro to instantiate a Pauli{N} type from a string
-        @p_str("IXYZ")
-        p"IXYZ..."
-    """
-    macro p_str(p)
-        conv = Dict(
-            'x' => [true, false],
-            'y' => [true, true],
-            'z' => [false, true],
-            'i' => [false, false]
-        )
-        m = SparseMatrixCSC(hcat([conv[lowercase(v)] for v in p]...)')
-        return Pauli(false, false, m)
+# Accessors
+xbits(p::Pauli) = p.xbits
+zbits(p::Pauli) = p.zbits
+bits(p::Pauli) = hcat(xbits(p), zbits(p))
+signbit(p::Pauli) = p.signbit
+imagbit(p::Pauli) = p.imagbit
+
+# In-Place Operations
+function mul!(C::Pauli{N}, A::Pauli{N}, B::Pauli{N}) where N 
+    C.signbit = A.signbit ⊻ B.signbit
+    C.imagbit = A.imagbit ⊻ B.imagbit
+    @inbounds for x in 1:N
+        C.signbit ⊻= signlogic(A.xbits[x], A.zbits[x], B.xbits[x], B.zbits[x])
+        C.imagbit ⊻= imaglogic(A.xbits[x], A.zbits[x], B.xbits[x], B.zbits[x])
+        C.xbits[x] = A.xbits[x] ⊻ B.xbits[x]
+        C.zbits[x] = A.zbits[x] ⊻ B.zbits[x]
     end
+end
 
-    """
-    @pauli XYZXII...
+mul!(A::Pauli{N}, B::Pauli{N}) where N = mul!(A, A, B)
 
-    Convenience macro for constructing a Pauli{N} type.
-    """
-    macro pauli(ex)
-        s = string(ex)
-        :(@p_str $s)
-    end
+function Base.:*(p::Pauli{N}, q::Pauli{N}) where N
+    o = Pauli{N}(false, false, similar(xbits(p)), similar(zbits(p)))
+    mul!(o, p, q)
+    o
+end
 
-    # todo: is it better to have a an empty struct or just pass around types
-    # abstract type PauliPrimitive end
-    # for x in "IXYZ"
-    #     s = Symbol("Pauli$(x)")
-    #     constname = Symbol(lowercase(x))
-    #     consttype = QuoteNode(s)
-    #     :(struct $s <: PauliPrimitive end) |> eval
-    #     :($constname = eval(Expr(:call, $consttype))) |> eval
-    # end
-    # Also, might help if the internals is binary? 
 
-    """
-    Alias for kronecker product 
-        
-        ⊗ === kron
-    """
-    const ⊗ = kron
-    function Base.kron(p1::Pauli, p2::Pauli) 
-        Pauli(
-            (p1.signbit ⊻ p2.signbit ⊻ (p1.imagbit & p2.imagbit)),
-            (p1.imagbit ⊻ p2.imagbit), 
-            vcat(p1.bits, p2.bits)
-        )
-    end
+"""
+Take a adjoint of the pauli operator `iXYZ...`  => `-iXYZ...`.
 
-    """
-    Return true if two paulis commute
-        $(FUNCTIONNAME)(x,y)
+    $(FUNCTIONNAME)(p::Pauli)
+"""
+Base.adjoint(p::Pauli) = p.imagbit ? Pauli(p.signbit ⊻ true, p.imagbit) : p
+
+
+
+# Scalar Multiplication
+*(y::Pauli, x::Number) = Pauli((x < 0) ⊻ y.signbit, y.imagbit, y.bits)
+*(y::Pauli, x::Complex) = isequal(x, im) ?
+                          Pauli(y.signbit ⊻ y.imagbit, ~y.imagbit, y.bits) : isequal(x, -im) ?
+                          Pauli(~y.signbit ⊻ y.imagbit, ~y.imagbit, y.bits) : throw(MethodError)
+*(x::Number, y::Pauli) = *(y, x)
+
+
+#### Negative sign -Pauli(1, [...]) = Pauli(-1, [...])
+Base.:-(x::Pauli) = Pauli(x.signbit ⊻ true, x.imagbit, x.bits)
+
+"""
+Return a new Pauli with the coefficient of `p` set to 1
+    abs(p::Pauli)
+"""
+abs(p::Pauli) = Pauli(false, false, p.bits)
+
+==(x::Pauli, y::Pauli) = (x.signbit == y.signbit) & isequal(bits(x), bits(y)) & (x.imagbit == y.imagbit)
+
+
+# todo: is it better to have a an empty struct or just pass around types
+# abstract type PauliPrimitive end
+# for x in "IXYZ"
+#     s = Symbol("Pauli$(x)")
+#     constname = Symbol(lowercase(x))
+#     consttype = QuoteNode(s)
+#     :(struct $s <: PauliPrimitive end) |> eval
+#     :($constname = eval(Expr(:call, $consttype))) |> eval
+# end
+# Also, might help if the internals is binary? 
+
+"""
+Alias for kronecker product 
     
-    """
-    commuting(x::Pauli, y::Pauli) = (x * y).imagbit & (x.imagbit ⊻ y.imagbit)
+    ⊗ === kron
+"""
+const ⊗ = kron
+function Base.kron(p1::Pauli, p2::Pauli)
+    Pauli(
+        (p1.signbit ⊻ p2.signbit ⊻ (p1.imagbit & p2.imagbit)),
+        (p1.imagbit ⊻ p2.imagbit),
+        vcat(p1.bits, p2.bits)
+    )
+end
 
-    """
-    Return true if two paulis anticommute
-        $(FUNCTIONNAME)(x,y)
-    """
-    anticommuting(x::Pauli, y::Pauli) = ~((x * y).imagbit) & (x.imagbit ⊻ y.imagbit)
+"""
+Return true if two paulis commute
+    $(FUNCTIONNAME)(x,y)
 
-    # commuting(x::AbstractVector{<:Pauli}, y::AbstractVector{<:Pauli}) = [commuting.(i,y) for i in x]
-    # anticommuting(x::AbstractVector{<:Pauli}, y::AbstractVector{<:Pauli}) = [anticommuting.(i,y) for i in x]
+"""
+commuting(x::AbstractPauli, y::AbstractPauli) = imagbit(x * y) & (imagbit(x) ⊻ imagbit(y))
 
-    # setting up iteration interface
-    Base.length(x::Pauli) = 1
-    Base.iterate(x::Pauli) = (x, nothing)
-    Base.iterate(x::Pauli, n::Nothing) = nothing
+"""
+Return true if two paulis anticommute
+    $(FUNCTIONNAME)(x,y)
+"""
+anticommuting(x::AbstractPauli, y::AbstractPauli) = ~commuting(x, y)
 
-    
-    include("representations.jl")
 
-    export Pauli, @p_str, @pauli
-    export bits, xbits, zbits, symplectic, checkmatrix
-    export ⊗, commuting, anticommuting
+
+# commuting(x::AbstractVector{<:Pauli}, y::AbstractVector{<:Pauli}) = [commuting.(i,y) for i in x]
+# anticommuting(x::AbstractVector{<:Pauli}, y::AbstractVector{<:Pauli}) = [anticommuting.(i,y) for i in x]
+
+# setting up iteration interface
+Base.length(x::Pauli) = 1
+Base.iterate(x::Pauli) = (x, nothing)
+Base.iterate(x::Pauli, n::Nothing) = nothing
+
+
+include("representations.jl")
+
+export Pauli, @p_str, @pauli
+export bits, xbits, zbits, symplectic, checkmatrix
+export ⊗, commuting, anticommuting
 end
--- src/paulitable.jl
+++ src/paulitable.jl
@@ -1,19 +1,34 @@
 # TODO Autogenerate levi-civita dictionary indexed by tuples and keep it in the namespace.
-ε = [
-    0 1 -1;
-    -1 0 1;
-    1 -1 0
-] * im
-
-global const paulibits = Dict(
+const global paulibits = Dict(
     (false, false) => 0,
     (true, false) => 1,
     (true, true) => 2,
     (false, true) => 3,
 )
 
-"""
-Levi Civita AntiSymmetric Function 
-    $(FUNCTIONNAME)(i,j)
-"""
-levicivita(i::AbstractArray, j::AbstractArray) =  iszero(i) | iszero(j) | (i==j) ? 1 : ε[paulibits[i...], paulibits[j...]]
+const global paulibits2 = Dict(
+    (false, false) => 1,
+    (true, false) => 2,
+    (true, true) => 3,
+    (false, true) => 4,
+)
+
+const global psm = Bool[
+    0 0 0 0
+    0 0 0 1
+    0 1 0 0
+    0 0 1 0
+]
+
+const global pim = Bool[
+    0 0 0 0
+    0 0 1 1
+    0 1 0 1
+    0 1 1 0
+]
+
+signlogic(i, j, k, l) = @inbounds psm[paulibits2[i, j], paulibits2[k, l]]
+imaglogic(i, j, k, l) = @inbounds pim[paulibits2[i, j], paulibits2[k, l]]
+
+signlogic(i, j) = psm[paulibits2[i[1], i[2]], paulibits2[j[1], j[2]]]
+imaglogic(i, j) = pim[paulibits2[i[1], i[2]], paulibits2[j[1], j[2]]]
